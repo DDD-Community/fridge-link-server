b3a565757e9f4d8aebb73ee1b859e8f00b4d9eff
jhkang1517 <jhkang1517@gmail.com>
2024-01-24 22:25:01 +0900

    Request, Response -> DTO 통합

--- src/main/kotlin/mara/server/domain/ingredient/IngredientDto.kt
+++ src/main/kotlin/mara/server/domain/ingredient/IngredientDto.kt
@@ -1,5 +1,12 @@
 package mara.server.domain.ingredient
 
+data class IngredientRequest(
+    val category: String,
+    val name: String,
+    val iconImage: String,
+    val expirationDays: Int
+)
+
 data class IngredientResponse(
     val ingredientId: Long,
     val category: String,
--- src/main/kotlin/mara/server/domain/ingredient/IngredientRequest.kt
+++
@@ -1,8 +0,0 @@
-package mara.server.domain.ingredient
-
-data class IngredientRequest(
-    val category: String,
-    val name: String,
-    val iconImage: String,
-    val expirationDays: Int
-)
