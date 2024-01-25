84770f21486b2910cbf6db45c3af10cd823beed4
jhkang1517 <jhkang1517@gmail.com>
2024-01-25 23:38:48 +0900

    friend 도메인 생성 및 친구관계(friendship) 엔티티 생성

---
+++ src/main/kotlin/mara/server/domain/friend/Friendship.kt
@@ -0,0 +1,29 @@
+package mara.server.domain.friend
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import mara.server.domain.user.User
+
+@Entity
+class Friendship(
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "from_user_id")
+    val fromUser: User,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "to_user_id")
+    val toUser: User,
+
+    var status: String
+    ) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "friendship_id", nullable = false)
+    val friendshipId: Long = 0L
+}
