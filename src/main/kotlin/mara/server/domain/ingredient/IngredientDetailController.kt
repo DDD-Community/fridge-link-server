f13b6521b5ee0e8d38e43cdaffacbefccabe7d38
jhkang1517 <jhkang1517@gmail.com>
2024-01-26 21:18:16 +0900

    /det -> /detail 명시화

--- src/main/kotlin/mara/server/domain/ingredient/IngredientDetailController.kt
+++ src/main/kotlin/mara/server/domain/ingredient/IngredientDetailController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/ingrs/det")
+@RequestMapping("/ingrs/detail")
 class IngredientDetailController(
     private val ingredientDetailService: IngredientDetailService
 ) {
--- src/main/kotlin/mara/server/domain/ingredient/IngredientDetailRepository.kt
+++ src/main/kotlin/mara/server/domain/ingredient/IngredientDetailRepository.kt
@@ -2,9 +2,8 @@ package mara.server.domain.ingredient
 
 import mara.server.domain.refrigerator.Refrigerator
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
-import java.util.*
+import java.util.Optional
 
 @Repository
 interface IngredientDetailRepository : JpaRepository<IngredientDetail, Long> {
