a97432fb5ff9ad1e5dbbe99d9ed8b75148b0345e
jhkang1517 <jhkang1517@gmail.com>
2024-01-31 23:01:20 +0900

    deleteFriendship PutMapping -> PostMapping 변경

--- src/main/kotlin/mara/server/domain/friend/FriendshipController.kt
+++ src/main/kotlin/mara/server/domain/friend/FriendshipController.kt
@@ -5,7 +5,6 @@ import mara.server.common.success
 import mara.server.domain.user.UserNameResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -26,7 +25,7 @@ class FriendshipController(
         return success(friendshipService.getFriendshipList())
     }
 
-    @PutMapping
+    @PostMapping
     fun deleteFriendship(@RequestBody friendshipDeleteRequest: FriendshipDeleteRequest): CommonResponse<String> {
         return success(friendshipService.deleteFriendship(friendshipDeleteRequest))
     }
