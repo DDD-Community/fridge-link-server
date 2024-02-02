d59ed445cf35b1ee54fee362be6150d5857e3dee
jhkang1517 <jhkang1517@gmail.com>
2024-02-02 23:47:58 +0900

    S3 upload 기능 구현

---
+++ src/main/kotlin/mara/server/config/s3/S3Config.kt
@@ -0,0 +1,32 @@
+package mara.server.config.s3
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class S3Config {
+    @Value("\${cloud.aws.credentials.accessKey}")
+    private lateinit var accesskey: String
+
+    @Value("\${cloud.aws.credentials.secretKey}")
+    private lateinit var secretKey: String
+
+    @Value("\${cloud.aws.region.static}")
+    private lateinit var region: String
+
+    @Bean
+    fun amazonS3Client(): AmazonS3Client {
+        val basicAWSCredentials: BasicAWSCredentials = BasicAWSCredentials(accesskey, secretKey)
+        return AmazonS3ClientBuilder
+            .standard()
+            .withRegion(region)
+            .withCredentials(AWSStaticCredentialsProvider(basicAWSCredentials))
+            .build()
+            as AmazonS3Client
+    }
+}
---
+++ src/main/kotlin/mara/server/domain/s3/S3Service.kt
@@ -0,0 +1,38 @@
+package mara.server.domain.s3
+
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.CannedAccessControlList
+import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.services.s3.model.PutObjectRequest
+import com.amazonaws.util.IOUtils
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import java.io.ByteArrayInputStream
+import java.util.UUID
+
+@Service
+class S3Service(
+    private val s3Client: AmazonS3Client,
+    @Value("\${cloud.aws.s3.bucket}")
+    private val bucket: String,
+    @Value("\${cloud.aws.s3.dir}")
+    private val dir: String
+) {
+    fun upload(file: MultipartFile, customDir: String? = dir): String {
+        val fileName = UUID.randomUUID().toString() + "-" + file.originalFilename
+        val objMeta = ObjectMetadata()
+
+        val bytes = IOUtils.toByteArray(file.inputStream)
+        objMeta.contentLength = bytes.size.toLong()
+
+        val byteArrayIs = ByteArrayInputStream(bytes)
+
+        s3Client.putObject(
+            PutObjectRequest(bucket, dir + fileName, byteArrayIs, objMeta)
+                .withCannedAcl(CannedAccessControlList.PublicRead)
+        )
+
+        return s3Client.getUrl(bucket, dir + fileName).toString()
+    }
+}
