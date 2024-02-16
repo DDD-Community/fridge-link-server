f3fe9f91740ae03cf9a1fd5ee13f8cbea7a1408a
blackswitch <jhkang1517@gmail.com>
2024-02-16 17:29:05 +0900

    @PageableDefault 제거

--- src/main/kotlin/mara/server/domain/friend/FriendRefrigeratorController.kt
+++ src/main/kotlin/mara/server/domain/friend/FriendRefrigeratorController.kt
@@ -5,8 +5,6 @@ import mara.server.common.success
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.springframework.data.domain.Sort
-import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -20,14 +18,8 @@ class FriendRefrigeratorController(
     @GetMapping("/recent")
     fun getRecentFriendRefrigeratorList(
         @Qualifier("userPageable")
-        @PageableDefault(
-            size = 5, sort = ["ingredientAddDate"], direction = Sort.Direction.DESC
-        )
         userPageable: Pageable,
         @Qualifier("ingredientPageable")
-        @PageableDefault(
-            size = 4, sort = ["addDate"], direction = Sort.Direction.DESC
-        )
         ingredientPageable: Pageable,
     ): CommonResponse<Page<FriendRefrigeratorResponse>> {
         return success(friendRefrigeratorService.getRecentFriendRefrigeratorList(userPageable, ingredientPageable))
