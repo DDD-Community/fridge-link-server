0c7e87348853b4bc682fc5df10043ef02119d74c
jhkang1517 <jhkang1517@gmail.com>
2024-02-18 18:29:16 +0900

    FriendRefrigerator API Documentation 추가

--- src/main/kotlin/mara/server/domain/friend/FriendRefrigeratorController.kt
+++ src/main/kotlin/mara/server/domain/friend/FriendRefrigeratorController.kt
@@ -1,5 +1,7 @@
 package mara.server.domain.friend
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import mara.server.common.CommonResponse
 import mara.server.common.success
 import org.springframework.web.bind.annotation.GetMapping
@@ -8,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/friend-refrigs")
+@Tag(name = "친구 냉장고", description = "친구 냉장고 API")
 class FriendRefrigeratorController(
     private val friendRefrigeratorService: FriendRefrigeratorService
 ) {
     @GetMapping("/recent")
+    @Operation(summary = "친구 냉장고 최신 근황 조회 API")
     fun getRecentFriendRefrigeratorList(): CommonResponse<List<FriendRefrigeratorResponse>> {
         return success(friendRefrigeratorService.getRecentFriendRefrigeratorList())
     }
