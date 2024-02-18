15c34c10bb9e7bc3f7945475f98a1527ea451c36
jhkang1517 <jhkang1517@gmail.com>
2024-02-18 18:27:47 +0900

    Friendship API Documentation 추가

--- src/main/kotlin/mara/server/domain/friend/FriendshipController.kt
+++ src/main/kotlin/mara/server/domain/friend/FriendshipController.kt
@@ -1,5 +1,7 @@
 package mara.server.domain.friend
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import mara.server.common.CommonResponse
 import mara.server.common.success
 import mara.server.domain.user.UserFriendResponse
@@ -11,26 +13,31 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/friendship")
+@Tag(name = "친구", description = "친구 API")
 class FriendshipController(
     private val friendshipService: FriendshipService
 ) {
 
     @PostMapping
+    @Operation(summary = "친구 맺기 API")
     fun createFriendship(@RequestBody friendshipRequest: FriendshipRequest): CommonResponse<String> {
         return success(friendshipService.createFriendship(friendshipRequest))
     }
 
     @GetMapping
+    @Operation(summary = "친구 조회 API")
     fun getFriendshipList(): CommonResponse<List<UserFriendResponse>> {
         return success(friendshipService.getFriendshipList())
     }
 
     @GetMapping("/count")
+    @Operation(summary = "친구 수 조회 API")
     fun getFriendshipCount(): CommonResponse<Long> {
         return success(friendshipService.getFriendshipCount())
     }
 
     @PostMapping("/delete")
+    @Operation(summary = "친구 삭제 API")
     fun deleteFriendship(@RequestBody friendshipDeleteRequestList: List<FriendshipDeleteRequest>): CommonResponse<String> {
         return success(friendshipService.deleteFriendship(friendshipDeleteRequestList))
     }
