2c7e8b115096a5ce2645c55f03cf05fade4ff733
blackswitch <jhkang1517@gmail.com>
2024-02-19 09:29:19 +0900

    queryDSL 설정 변경

--- src/main/kotlin/mara/server/config/querydsl/QueryDslConfig.kt
+++ src/main/kotlin/mara/server/config/querydsl/QueryDslConfig.kt
@@ -2,14 +2,13 @@ package mara.server.config.querydsl
 
 import com.querydsl.jpa.impl.JPAQueryFactory
 import jakarta.persistence.EntityManager
-import jakarta.persistence.PersistenceContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class QueryDslConfig {
-    @PersistenceContext
-    lateinit var em: EntityManager
+class QueryDslConfig(
+    private val em: EntityManager
+) {
 
     @Bean
     fun jpaQueryFactory(): JPAQueryFactory {
