36342ddc6cf38b4205d3cee2f34deaa0a53b6c69
jhkang1517 <jhkang1517@gmail.com>
2024-02-19 23:35:40 +0900

    / -> /health-check 변경

--- src/main/kotlin/mara/server/domain/HealthController.kt
+++ src/main/kotlin/mara/server/domain/HealthController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HealthController {
 
-    @GetMapping("/")
+    @GetMapping("/health-check")
     fun healthCheck(): HttpStatus {
         return HttpStatus.OK
     }
