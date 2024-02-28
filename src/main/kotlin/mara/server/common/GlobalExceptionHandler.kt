23cae2442ba454319c1a87cb5e8bb34622a0bb9c
Jiwon-cho <gwon188@gmail.com>
2024-02-29 00:40:55 +0900

    refactor: add exception data

--- src/main/kotlin/mara/server/common/GlobalExceptionHandler.kt
+++ src/main/kotlin/mara/server/common/GlobalExceptionHandler.kt
@@ -9,9 +9,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 @ControllerAdvice
 class GlobalExceptionHandler {
     val log = logger()
+
     @ExceptionHandler(InvalidDeployStatusException::class)
-    fun handleInvalidDeployStatusException(ex: InvalidDeployStatusException): ResponseEntity<CommonResponse<Any>> {
+    fun handleBadRequestException(ex: InvalidDeployStatusException): ResponseEntity<CommonResponse<Any>> {
         log.warn("[{}] handled: {}", ex.javaClass.simpleName, ex.message)
+
+        /**
+         * 차후 확장성을 고려하여 하단의 주석 처럼 수정 할 수 있음
+         *
+         * val httpStatus = when (ex) {
+         *         is InvalidDeployStatusException -> HttpStatus.BAD_REQUEST
+         *         is AnotherCustomException -> HttpStatus.INTERNAL_SERVER_ERROR
+         *         else -> HttpStatus.INTERNAL_SERVER_ERROR // 기본적으로 설정할 HTTP 상태 코드
+         *     }
+         * */
+
         val httpStatus = HttpStatus.BAD_REQUEST
         val data = ex.additionalData
         return fail(ex.message ?: "An error occurred", httpStatus, data)
